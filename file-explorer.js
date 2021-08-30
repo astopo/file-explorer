f179ce4c398d176a5620cc79dc35247e0619ee35
astopo <amy.soetopo@gmail.com>
2021-08-30 13:54:04 +0200

    main: minor cleanup, comments

--- file-explorer.js
+++ file-explorer.js
@@ -5,6 +5,7 @@ const watcher = new Watcher({ onFileAdded, onFileDeleted, onDirectoryAdded, onDi
 watcher.watch()
 
 
+// Server setup
 const express = require('express')
 const app = express()
 const http = require('http')
@@ -12,19 +13,26 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
+// Serve static files from public directory
+app.use(express.static('public'))
+
+// Set the default route to file-explorer.html
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/file-explorer.html');
+  res.sendFile(__dirname + '/public/file-explorer.html')
 })
 
+// Have the websocket server ready to handle new connections
 io.on('connection', (socket) => {
   console.log('a user connected')
   // Send the current tree and directoryPaths.
   socket.emit('init', { tree: watcher.tree, directoryPaths: watcher.directoryPaths })
 })
 
+// Start the server.
 server.listen(3000, () => {
   console.log('Server listening on *:3000');
 })
+
 
 // Callbacks for watcher that emit the changes to the client.
 
